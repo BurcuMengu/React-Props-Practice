d0df2f902a84797f736673326a6141c4eac22bad
Burcu <112901565+BurcuMengu@users.noreply.github.com>
2024-09-18 16:57:36 +0300

    Initial commit

---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+const contacts = [
+  {
+    name: "Turanga Leela",
+    imgURL: "https://upload.wikimedia.org/wikipedia/tr/d/d4/Turanga_Leela.png",
+    phone: "+123 456 789",
+    email: "t@turanga.com",
+  },
+  {
+    name: "Philip J. Fry",
+    imgURL:
+      "https://images2.fanpop.com/image/photos/9400000/Fry-of-Futurama-philip-j-fry-9424600-400-474.jpg",
+    phone: "+987 654 321",
+    email: "p@philip.com",
+  },
+  {
+    name: "Bender",
+    imgURL:
+      "https://e7.pngegg.com/pngimages/307/360/png-clipart-bender-futurama-season-1-jake-the-dog-bender-hand-cartoon.png",
+    phone: "+918 372 574",
+    email: "b@bender.com",
+  },
+];
+
+export default contacts;
